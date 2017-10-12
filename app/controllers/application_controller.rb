a29b0464d3a0a0cd580453f547124d0aea3c04fa
Cecily2 <cecily.downs@gmail.com>
2017-10-12 16:49:49 -0700

    Remove code that didn't work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
 
@@ -8,11 +7,6 @@ class ApplicationController < ActionController::Base
 
   
 
-  protected
-  def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end 
+
   
 end
