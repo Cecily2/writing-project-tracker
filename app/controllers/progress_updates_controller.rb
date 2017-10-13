0d2402956a689176bc6b0767f409784c1cbc186d
Cecily2 <cecily.downs@gmail.com>
2017-10-13 13:31:57 -0700

    Progress update new route

--- app/controllers/progress_updates_controller.rb
+++ app/controllers/progress_updates_controller.rb
@@ -2,4 +2,9 @@ class ProgressUpdatesController < ApplicationController
   def show
     @update = ProgressUpdate.find(params[:id])
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @update = ProgressUpdate.new
+  end
 end
