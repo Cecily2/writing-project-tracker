0f4c4eece9b460b297bd60f4099ef227e2a3006a
Cecily2 <cecily.downs@gmail.com>
2017-10-12 17:00:27 -0700

    Make usernames work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
