b091132f6eb8211f1138a8ad86c7a3db4cd8d667
Cecily2 <cecily.downs@gmail.com>
2017-10-13 16:52:39 -0700

    Change time:integer to hours:float

--- db/migrate/20171013000716_create_projects.rb
+++ db/migrate/20171013000716_create_projects.rb
@@ -6,7 +6,7 @@ class CreateProjects < ActiveRecord::Migration[5.1]
       t.string :cover_art
       t.integer :word_goal
       t.integer :words, default: 0
-      t.integer :time, default: 0
+      t.float :hours, default: 0
       t.integer :user_id
 
       t.timestamps
