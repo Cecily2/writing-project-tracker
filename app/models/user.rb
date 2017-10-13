a1dae3bd9b96111c06510fde0e837a425f65c511
Cecily2 <cecily.downs@gmail.com>
2017-10-12 17:38:11 -0700

    Set up user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :projects
+  has_many :updates
+  has_many :comments
+  has_many :commented_updates, as: :updates, through: :comments
 
   validates :username, presence: true
   validates :username, uniqueness: true
