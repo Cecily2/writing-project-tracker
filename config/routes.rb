bbfd5c665403bf20416939c630e8abce0443f92d
Cecily2 <cecily.downs@gmail.com>
2017-10-13 12:49:38 -0700

    Nest updates under projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :progress_updates, :path => 'updates'
-  resources :projects
+  resources :projects do
+    resources :progress_updates, :path => 'updates'
+  end
 
-
-  
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
