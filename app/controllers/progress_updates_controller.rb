5dfdfb364182e10bce175d41c91e6835bc4c4b69
Cecily2 <cecily.downs@gmail.com>
2017-10-13 16:55:49 -0700

    Add progress update create route and related class method

--- app/controllers/progress_updates_controller.rb
+++ app/controllers/progress_updates_controller.rb
@@ -1,10 +1,35 @@
+require "pry"
+
 class ProgressUpdatesController < ApplicationController
   def show
     @update = ProgressUpdate.find(params[:id])
   end
 
   def new
-    @project = Project.find(params[:project_id])
-    @update = ProgressUpdate.new
+    if current_user
+      @project = Project.find(params[:project_id])
+      @update = ProgressUpdate.new
+    else
+      redirect_to "/users/sign_in"
+    end
   end
+
+  def create
+    project = Project.find(params[:project_id])
+    update = ProgressUpdate.add_and_update_project(update_params, params[:word_format], project)
+    update.user = current_user
+    if update.save
+      update.project.save
+      redirect_to project_path(project)
+    else
+      redirect_to "/"
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:progress_update).permit(:content, :words, :hours)
+  end
+
 end
--- app/models/progress_update.rb
+++ app/models/progress_update.rb
@@ -1,5 +1,25 @@
+require 'pry'
 class ProgressUpdate < ApplicationRecord
   belongs_to :project
   belongs_to :user
   has_many :comments
+
+  def self.add_and_update_project(params, word_format, project)
+    update = self.new
+    update.project = project
+    if word_format == "new"
+      update.project.words += params[:words].to_i
+      words_added = params[:words].to_i
+    else
+      update.project.words = params[:words].to_i
+      words_added = update.project.words - params[:words].to_i
+    end
+
+
+    update.project.hours += params[:hours].to_f
+    update.content = params[:content]
+    update.words = words_added
+    update.hours = params[:hours].to_f
+    update
+  end
 end
