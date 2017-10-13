58e6292baedad4a89f46ee8221e28624f2007c44
Cecily2 <cecily.downs@gmail.com>
2017-10-12 18:42:40 -0700

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   resources :comments
   resources :updates
   resources :projects
-  resources :users, only: [:index, :show]
+
+
+  
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 
+ resources :users, only: [:show, :index] 
+  
   root "application#index"
 
   
