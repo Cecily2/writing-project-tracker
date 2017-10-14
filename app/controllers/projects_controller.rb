6e815ef21c7d80e65897ea5f829cd4f3ba2ced22
Cecily2 <cecily.downs@gmail.com>
2017-10-14 15:48:34 -0700

    Add project New page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+    @project.genres.build
+  end
 end
