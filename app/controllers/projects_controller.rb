9b85040c45ff7fec21603bdc7e364f9fa09204ac
Cecily2 <cecily.downs@gmail.com>
2017-10-14 17:59:04 -0700

    Make new project form properly add existing genres

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
+require 'pry'
 class ProjectsController < ApplicationController
+
   def show
     @project = Project.find(params[:id])
   end
@@ -7,4 +9,24 @@ class ProjectsController < ApplicationController
     @project = Project.new
     @project.genres.build
   end
+
+  def create
+    project = Project.new(project_params)
+    project.type = Type.find(params[:project][:type])
+    project.user = current_user
+    if project.save
+      redirect_to project_path(project)
+    else
+      redirect_to "/"
+    end
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :word_goal, :words, :hours, :genre_ids => [])
+  end
+
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,7 @@ belongs_to :type
 has_many :project_genres
 has_many :genres, through: :project_genres
 
+accepts_nested_attributes_for :genres
+
+
 end
