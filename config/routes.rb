d509899f8450129536116730175b6de5409cb3d7
Cecily2 <cecily.downs@gmail.com>
2017-10-14 19:52:23 -0700

    Facebook users don't have to enter password to edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :progress_updates, :path => 'updates'
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
