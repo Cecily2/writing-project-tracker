fb917edf3c0b30eab8feec9cdcfa245d40a0a69a
Cecily2 <cecily.downs@gmail.com>
2017-10-17 21:25:17 -0700

    Set up user-genre associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :progress_updates
   has_many :comments
   has_many :commented_updates, as: :progress_updates, through: :comments
+  has_many :user_genres
+  has_many :genres, through: :user_genres
 
   validates :name, presence: true
 
