38d065cf45606c13a3328b7564b6f6cb8872fc33
Cecily2 <cecily.downs@gmail.com>
2017-10-19 16:45:51 -0700

    Move edit and update links to helper

--- app/helpers/updates_helper.rb
+++ app/helpers/updates_helper.rb
@@ -1,2 +1,9 @@
 module UpdatesHelper
+  def edit_update_link(update)
+    link_to "Edit Update", [:edit, update.project, update], class: "btn btn-sm btn-primary"  if can? :update, update
+  end
+
+  def delete_update_link(update)
+    link_to 'Delete Update', [update.project, update], method: :delete, class: "btn btn-sm btn-danger" if can? :destroy, update
+  end
 end
