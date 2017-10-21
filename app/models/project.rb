8871c0021fab176f1004169352a2be44f455b032
Cecily2 <cecily.downs@gmail.com>
2017-10-21 13:12:02 -0700

    Refactor

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,9 +8,11 @@ class Project < ApplicationRecord
   validates :title, presence: true
 
   def genres_attributes=(genre_attributes)
-    genre_attributes.values.each do |genre_attribute|
-      genre = Genre.find_or_create_by(genre_attribute)
-      self.genres << genre unless genre_attribute[:name] == ""
+    if genre_attributes[:name] != ""
+      genre_attributes.values.each do |genre_attribute|
+        genre = Genre.find_or_create_by(genre_attribute)
+        self.genres << genre
+      end
     end
   end
 
