895b1c61c150cc13c0cf6c06840869078c168595
Cecily2 <cecily.downs@gmail.com>
2017-10-22 14:38:08 -0700

    Adjust avatar thumbnail size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -49,7 +49,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    resize_to_fill(80, 80)
+    resize_to_fill(150, 150)
   end
 
   version :normal do
