2847f8f218549352888ca084dc68cb2c5f5f1b7a
Cecily <cecily.downs@gmail.com>
2017-11-09 19:53:27 -0800

    Remove unused associations from serializer and add custom methods

--- app/serializers/progress_update_serializer.rb
+++ app/serializers/progress_update_serializer.rb
@@ -1,6 +1,24 @@
 class ProgressUpdateSerializer < ActiveModel::Serializer
-  attributes :id, :content, :words, :hours
-  belongs_to :user, serializer: ProgressUpdateUserSerializer
-  belongs_to :project, serializer: ProgressUpdateProjectSerializer
+  include ActionView::Helpers::DateHelper  
+  attributes :id, :content, :words, :hours, :prev_link, :next_link, :created_at_in_words
+  # belongs_to :user, serializer: ProgressUpdateUserSerializer
+  # belongs_to :project, serializer: ProgressUpdateProjectSerializer
   has_many :comments
+
+  def created_at_in_words
+    time_ago_in_words(object.created_at) + " ago"
+  end
+
+  def prev_link
+    if prev_object = object.prev
+      "/projects/#{prev_object[:project_id]}/updates/#{prev_object.id}"
+    end
+  end
+
+  def next_link
+    if next_object = object.next
+      "/projects/#{next_object[:project_id]}/updates/#{next_object.id}"
+    end
+  end
+
 end
