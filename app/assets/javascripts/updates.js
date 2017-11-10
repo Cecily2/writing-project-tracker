5c9ba371fa1f8b51e53e63766af5fc8df52e1487
Cecily <cecily.downs@gmail.com>
2017-11-09 19:54:04 -0800

    Add basic prev/next update JS functionality

--- app/assets/javascripts/updates.js
+++ app/assets/javascripts/updates.js
@@ -3,65 +3,46 @@ function Update(update){
     this.content = update.content
     this.words = update.words
     this.hours = update.hours
-    this.user = {
-        id: update.user.id,
-        name: update.user.name,
-        avatar: update.user.avatar.thumb.url,
-        gender: update.user.gender
-    }
-    this.project = {
-        id: update.project.id,
-        title: update.project.title
-    }
+    this.created_at = update.created_at_in_words
+    this.prev_link = update.prev_link
+    this.next_link = update.next_link
+    
     this.comments = update.comments
 }
 
-Update.prototype.format = function(){
-    // DO I WANT TO REPLACE ALL HTML AT ONCE?
-    // Or should I have multiple template functions, to update things more selectively?
-    return ``   
+Update.prototype.nextLinkHTML = function(){
+    if(this.next_link){
+        return `<a class="next-prev-link" href="${this.next_link}">Next</a>`   
+    } else {
+        return ""
+    }
+}
+
+Update.prototype.prevLinkHTML = function(){
+    if(this.prev_link){
+        return `<a class="next-prev-link" href="${this.prev_link}">Previous</a>`   
+    } else {
+        return ""
+    } 
 }
 
 $(document).on('turbolinks:load', function(){
-    // SO I'd want to add listeners to 'previous' and 'next' links.
-    // GET IT WORKING WITHOUT COMMENTS FIRST.
-
 
     $(".next-prev-link").click(function(e){
             e.stopImmediatePropagation()
             e.preventDefault()
             $.get(this.href + ".json", function(data){
-                console.log(data)
+                let newUpdate = new Update(data)
+                $(".created-at").text(newUpdate.created_at)
+                $(".progress-update-content").text(newUpdate.content)
+                $(".next").html(newUpdate.nextLinkHTML())
+                $(".prev").html(newUpdate.prevLinkHTML())   
+                    
+                // STUFF I STILL NEED TO UPDATE:
+                // 1. Words and hours.
+                // 2. Comments & new comment form.
+                // 3. Edit & delete links if they're visible.
             })
     })
-
-
-    // $(".new_comment").on("submit", function(e) {
-    //     e.preventDefault()
-    //     e.stopImmediatePropagation()        
-
-    //     $.post(this.action, $(this).serialize(), function(comment) {
-    //         let newComment = new Comment(comment)
-    //         let newCommentHTML = newComment.format()
-    //         $(".list-unstyled").append(newCommentHTML)
-    //         $("#comment_content").val("")
-    //     })
-    // })
-
-    // $(".delete-comment").click(function(e) {
-    //     e.stopImmediatePropagation()
-    //     e.preventDefault()
-    //     commentId = this.getAttribute("data-comment-id")
-
-    //     $.ajax({
-    //         type: 'DELETE',
-    //         beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},   
-    //         url: this.href,
-    //         success: function(e) {
-    //             removeComment(commentId)
-    //         }
-    //     });
-    // })
-
 
 })
