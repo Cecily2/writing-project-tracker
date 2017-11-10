ffddfb5f9c6b44073588c3bdb0e3c0a52f6a9eb5
Cecily <cecily.downs@gmail.com>
2017-11-09 17:30:41 -0800

    Add working remove comment link to comments added with ajax

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -9,19 +9,23 @@ function Comment(comment){
 }
 
 Comment.prototype.format = function(){
-    return `<li class="media">
+    return `<li class="media" id="comment-${this.id}">
     <img class="d-flex mr-3 avatar-xs" src="${this.user.avatar}">
     <div class="media-body">
       ${this.content}<br>
       <small class="text-muted">
         <a href="/users/${this.user.id}">${this.user.name}</a>
           - Just now
+          <a class="delete-comment" data-comment-id="${this.id}" rel="nofollow" data-method="delete" href="${window.location.href}/comments/${this.id}" onClick="removeComment(${this.id})">Delete Comment</a>
       </small>
       <br><br>      
     </div>
     </li>`   
 }
 
+function removeComment(id){
+    $("#comment-" + id).remove()
+}
 
 $(document).on('turbolinks:load', function(){
 
@@ -40,15 +44,14 @@ $(document).on('turbolinks:load', function(){
     $(".delete-comment").click(function(e) {
         e.stopImmediatePropagation()
         e.preventDefault()
-
-        let commentId = this.getAttribute("data-comment-id")
+        commentId = this.getAttribute("data-comment-id")
 
         $.ajax({
             type: 'DELETE',
             beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},   
             url: this.href,
             success: function(e) {
-                $("#comment-" + commentId).remove()
+                removeComment(commentId)
             }
         });
     })
