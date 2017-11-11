21cf20b0b00a08f36cac79e93fbe83efb78ee68f
Cecily <cecily.downs@gmail.com>
2017-11-10 16:23:54 -0800

    Render new data from ajax progress update show response

--- app/assets/javascripts/updates.js
+++ app/assets/javascripts/updates.js
@@ -6,35 +6,57 @@ function Update(update){
     this.created_at = update.created_at_in_words
     this.previous_link = update.previous_link
     this.next_link = update.next_link
-    
+    this.project_id = update.project.id
     this.comments = update.comments
 }
 
-Update.prototype.LinkHTML = function(direction){
-    console.log(this[direction + "_link"])
+Update.prototype.linkHTML = function(direction){
     if(this[direction + "_link"]){
-        return `<a class="next-prev-link" href="${this[direction + "_link"]}">${direction.charAt(0).toUpperCase() + direction.slice(1)}</a>`   
+        return `<a class="next-prev-link" href="${this[direction + "_link"]}" onClick="return false">${direction.charAt(0).toUpperCase() + direction.slice(1)}</a>`   
     } else {
         return ""
     }
 }
 
-$(document).on('turbolinks:load', function(){
+Update.prototype.commentSubmitLink = function(direction){
+   return `/projects/${this.project_id}/updates/${this.id}/comments`
+}
 
-    $(".next-prev-link").click(function(e){
+Update.prototype.statsHTML = function(direction){
+    let template = ""
+    if(this.words){
+        template += `<div class="col"><span>${this.words}</span> words added</div>`
+    }
+    if(this.hours){
+        template += `<div class="col"><span>${this.hours}</span> hours worked</div>`
+    }
+    return template
+}
+
+$(document).on('turbolinks:load', function(){
+    $("#next-prev-navigation").on('click', '.next-prev-link', function(e){    
+        // using the above selector so it works with elements that have been added with ajax
             e.stopImmediatePropagation()
             e.preventDefault()
             $.get(this.href + ".json", function(data){
+                
                 let newUpdate = new Update(data)
                 $(".created-at").text(newUpdate.created_at)
                 $(".progress-update-content").text(newUpdate.content)
-                $(".next").html(newUpdate.LinkHTML("next"))
-                $(".previous").html(newUpdate.LinkHTML("previous"))   
-                    
-                // STUFF I STILL NEED TO UPDATE:
-                // 1. Words and hours.
-                // 2. COMMENTS & new comment form.
-                // 3. Edit & delete links if they're visible.
+                $(".next").html(newUpdate.linkHTML("next"))
+                $(".previous").html(newUpdate.linkHTML("previous"))
+                $(".stats .row").html(newUpdate.statsHTML()) 
+                $(".new_comment").attr('action', newUpdate.commentSubmitLink())                
+
+                // 3. Change edit and delete links if they're visible.
+
+                $(".list-unstyled").html("")
+                newUpdate.comments.forEach(function(item){
+                    let comment = new Comment(item)
+                    let commentHTML = comment.format()
+                    $(".list-unstyled").append(commentHTML)
+                })
+
             })
     })
 
